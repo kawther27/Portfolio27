0413efe74b65c91fe274e59220efee307af655df
kawther27 <kawtherkhlif20@gmail.com>
2024-07-12 20:06:16 -0400

    adding features

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Contact from './pages/Contact.jsx'
 function App() {
 
 
   return (
     <>
-    
+    <Contact></Contact>
     </>
   )
 }
