8dd064704583b8e2d04ae996650fd0e99e2e1c58
kawther27 <kawtherkhlif20@gmail.com>
2024-07-15 15:25:50 -0400

    Adding style and component to home-page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import Typewriter from 'typewriter-effect';
+import '../styles/Home.css'
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home-container">
+          <h1 className="hero-title">
+            <Typewriter
+              options={{
+                strings: ['I Am Web Designer.'],
+                autoStart: true,
+                loop: true,
+                cursor: '|',
+                delay: 75,
+              }}
+            />
+          </h1>
+          <div className="scroll-down">
+            <a href="#about" className="scroll-link">
+              <img src="./src/assets/images/down-arrow.png" alt="Scroll Down" className="scroll-icon" />
+            </a>
+          </div>
+        </div>
+   
+   
   )
 }
 
