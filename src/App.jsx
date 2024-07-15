c6650361575585e05524c1d6df58094496ac4e56
kawther27 <kawtherkhlif20@gmail.com>
2024-07-15 18:46:16 -0400

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from './components/layout';
+import About from './pages/About';
 
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
 
   return (
   
-<div>
+<About>
   
-</div>
+</About>
 
    );
 
