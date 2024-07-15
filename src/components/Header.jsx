55b76d4475cd0b292335eb2214b7864dc2bf0e17
kawther27 <kawtherkhlif20@gmail.com>
2024-07-14 21:53:52 -0400

    adding responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,17 @@ function Header() {
   return (
     <header>
     <Navbar className="custom-header"  expand="lg" fixed="top">
-      <Container 
-       style={{ 
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          padding: 0 , margin: 0 }}>
-        <Navbar.Brand href="#home" className='mx-5'>Portfolio</Navbar.Brand>
-        
-          <Nav className="ml-auto" 
-                style={{
-                display: 'flex',
-                flexDirection: 'row'
-                   }}>
-            <Nav.Link href="#home" className=" text-white nav-link-custom mx-3" >Home</Nav.Link>
-            <Nav.Link href="#projects" className="  text-white nav-link-custom  mx-3">Projects</Nav.Link>
-            <Nav.Link href="#contact" className=" text-white nav-link-custom  mx-3">Contact</Nav.Link>
-            <Nav.Link href="#testimonials" className=" text-white nav-link-custom  mx-3">Testimonials</Nav.Link>
-          </Nav>
+      <Container >
+        <Navbar.Brand href="#home" style={{ color: '#d6bd97' }} className=' mx-4'>Portfolio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="sm-basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto flex-sm-row align-items-start  ">
+              <Nav.Link href="#home" className="text-white nav-link-custom mx-4">Home</Nav.Link>
+              <Nav.Link href="#projects" className="text-white nav-link-custom mx-4">Projects</Nav.Link>
+              <Nav.Link href="#contact" className="text-white nav-link-custom mx-4">Contact</Nav.Link>
+              <Nav.Link href="#testimonials" className="text-white nav-link-custom mx-4">Testimonials</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
       </Container>
     </Navbar>
   </header>
