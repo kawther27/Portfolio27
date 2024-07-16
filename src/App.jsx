860a267a2e7d87115c900ad3ec9148106a5c429b
kawther27 <kawtherkhlif20@gmail.com>
2024-07-16 15:06:43 -0400

    adding fonctinalty and style to project-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css'
+import Projects from './pages/Projects'
 
 function App() {
 
 
   return (
     <>
-    
+    <Projects></Projects>
     </>
   )
 }
