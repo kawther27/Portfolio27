e1c60c6395c2d7c70a2f746ac3b6ac3c026e7b0c
kawther27 <kawtherkhlif20@gmail.com>
2024-07-16 21:30:03 -0400

    adding fonctionalty and style to testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Testimonials from './pages/Testimonials';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 
   return (
     <>
-    
+      <Testimonials>
+        
+      </Testimonials>
     </>
   )
 }
